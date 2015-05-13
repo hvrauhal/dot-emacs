355e797e6ed48da5a87ce99422ae2aa997221856
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2015-05-13 12:28:51 +0300

    Set themes for terminal and window-system versions

--- init.el
+++ init.el
@@ -68,8 +68,12 @@
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
-(when window-system (set-exec-path-from-shell-PATH))
-
+(if window-system
+  (progn
+    (set-exec-path-from-shell-PATH)
+    (load-theme 'leuven t))
+  (progn
+    (load-theme 'hipster t)))
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
