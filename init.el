a1b027978d78b9df8e31660d09108ac2b47ca638
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2015-05-13 11:17:33 +0300

    Current init.el

---
+++ init.el
@@ -0,0 +1,114 @@
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+
+(package-initialize)
+
+
+(defvar my-packages '(rainbow-delimiters
+                      clojure-mode
+                      clojure-test-mode
+                      markdown-mode
+                      cider
+                      regex-tool
+                      js2-mode
+                      better-defaults
+                      org))
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
+
+(setq inhibit-splash-screen t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(css-indent-offset 2)
+ '(current-language-environment "UTF-8")
+ '(indent-tabs-mode nil nil nil "Use spaces only.")
+ '(initial-scratch-message nil)
+ '(nxml-child-indent 2)
+ '(nxml-outline-child-indent 2)
+ '(org-agenda-files (quote ("~/y/diary.org")))
+ '(sgml-basic-offset 4)
+ '(show-paren-mode t)
+ '(transient-mark-mode (quote identity))
+ '(whitespace-style (quote (face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark newline-mark))))
+
+
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode 'nil))
+
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+
+(global-set-key (kbd "M-S-SPC") 'hippie-expand)
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
+
+;; fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+(put 'narrow-to-region 'disabled nil)
+
+(set-face-attribute 'default nil
+                    :family "Source Code Pro" :height 130 :weight 'normal)
+
+
+(defadvice hl-line-mode (after
+                         dino-advise-hl-line-mode
+                         activate compile)
+  (set-face-background 'hl-line "#f8f8f8"))
+;(global-hl-line-mode)
+
+(defun my-js2-mode-toggle-strict-missing-semi-warning ()
+  (interactive)
+  (setq js2-strict-missing-semi-warning (eq js2-strict-missing-semi-warning nil))
+  (js2-mode))
+
+(setq js-indent-level 2)
+
+
+; (delete-selection-mode 1)
+(set-default-font "Source Code Pro 13")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+  ;; The following lines are always needed.  Choose your own keys.
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+ (load (expand-file-name "~/quicklisp/slime-helper.el"))
+  ;; Replace "sbcl" with the path to your implementation
+  (setq inferior-lisp-program "sbcl")
+
